e2a9ca1c639d8fcce5501a4ee3c8ff248342bc61
studemark <studemark@gmail.com>
2019-04-25 10:40:29 -0500

    Added Parser and ExpressionNode

---
+++ Parser.java
@@ -0,0 +1,16 @@
+import java.util.LinkedList;
+
+public class Parser {
+	LinkedList<Token> tokenList;
+	
+	Parser(LinkedList<Token> tokens) {
+		LinkedList<Token> tokenList = tokens;
+		
+	}
+	
+	Token currentToken = tokenList.getFirst();
+	
+//	public Node Parse(tokenList) {
+//		
+//	}
+}
